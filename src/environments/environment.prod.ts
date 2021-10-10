33a68e021a8737f3d9b92d8db1c13afc7630999c
Leifer Mendez <leifer33@gmail.com>
2021-10-10 12:37:30 +0200

    set api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://api-10k.herokuapp.com',
+  api: 'https://api-10k.herokuapp.com/api/1.0',
   videoMain: 'https://www.youtube.com/watch?v=4N-SYyu1fY8'
 };
