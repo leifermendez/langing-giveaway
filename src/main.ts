a3e928977aece64c6a1c56e8a345d3ddac4ed707
Leifer Mendez <leifer33@gmail.com>
2021-10-10 12:11:15 +0200

    remove log

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import {environment} from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  if (window) {
+    window.console.log = window.console.warn = window.console.info = () => {
+      // Don't log anything.
+    };
+    window.console.error = ($event, more) => {};
+  }
+
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
